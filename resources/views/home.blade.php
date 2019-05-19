d609a1b00eacb5b2e9a7560b62433e89e0bc5084
zvvysotskaya <zhannavyvy@gmail.com>
2019-05-18 22:19:19 -0400

    a home page changed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,89 +10,101 @@
                     <h3>Hello {{ Auth::user()->name }}</h3>
 <!--There are buttons to access created items-->
 <div class="container-fluid">
-  <div class="row">
-    <div class="col-lg-4 col4created">
-      <a href="{{ url('userpages.shoppinglist') }}">
-        <div class="col-lg-6 col12created1 text-center">
-          shopping list
+    <div class="row">
+        <div class="col-lg-4 col4created">
+            <div class="text-center">
+                <a href="{{ url('userpages.shoppinglist') }}" class="btn btn-lg btn1 btn-block">
+                    shopping list
+                </a>
+            </div>
         </div>
-      </a>
-    </div>
-    <div class="col-lg-4 col4created">
-      <a href="{{ url('userpages.chorelist') }}">
-        <div class="col-lg-6 col12created2 text-center">
-          chore list
+        <div class="col-lg-4 col4created">
+            <div class="text-center">
+                <a href="{{ url('userpages.chorelist') }}" class="btn btn-lg btn2 btn-block">
+                    chore list
+                </a>
+            </div>
         </div>
-      </a>
+        <div class="col-lg-4 col4created">
+            <div class="text-center">
+                <a href="#" class="btn btn-lg btn3 btn-block">
+                    something else
+                </a>
+            </div>
+        </div>
     </div>
-    <div class="col-lg-4 col4created">
-      <div class="col-lg-6 col12created1 text-center">
-        something else
-      </div>
-    </div>
-  </div>
 </div>
-<br>
+
 <!--Create items-->
 
 <div class="container-fluid">
-  <div class="row">
-    <div class="col-lg-4 col4create">
-      <div class="col-lg-12 col12create text-center">
-        <h4>Create a New Shopping List</h4>
-        <br><br>
-        <div class="dropdown">
-          <button class="btn btn-info dropdown-toggle" type="button" data-toggle="dropdown">Categories
-          <span class="caret"></span></button>
-          <ul class="dropdown-menu">
-            <li class="text-center"><a href="{{ url('userpages.shoppinglistgroceries') }}">Groceries</a></li>
-            <li class="text-center"><a href="{{ url('userpages.shoppinglisthouseholditems') }}">Household Items</a></li>
-            <li class="text-center"><a href="{{ url('userpages.shoppinglistclothes') }}">Clothes</a></li>
-          </ul>
+    <div class="row">
+        <div class="col-lg-4 col4create">
+            <div class="col-lg-12 col12create text-center">
+                <h4>Create a New Shopping List</h4>
+                <br><br>
+                <div class="dropdown">
+                    <button class="btn btn-info dropdown-toggle" type="button" data-toggle="dropdown">
+                        Categories
+                        <span class="caret"></span>
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li class="text-center"><a href="{{ url('userpages.shoppinglistgroceries') }}">Groceries</a></li>
+                        <li class="text-center"><a href="{{ url('userpages.shoppinglisthouseholditems') }}">Household Items</a></li>
+                        <li class="text-center"><a href="{{ url('userpages.shoppinglistclothes') }}">Clothes</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
-      </div>
+        <div class="col-lg-4 col4create">
+            <div class="col-lg-12 col12create text-center">
+                <h4>Create a New Chore List</h4>
+                <br><br>
+                <div class="dropdown">
+                    <button class="btn btn-info dropdown-toggle" type="button" data-toggle="dropdown">
+                        Categories
+                        <span class="caret"></span>
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li class="text-center"><a href="{{ url('userpages.chorelisthome') }}">Household</a></li>
+                        <li class="text-center"><a href="{{ url('userpages.chorelistjob') }}">Job</a></li>
+                        <li class="text-center"><a href="{{ url('userpages.chorelistentertainment') }}">Entertainment</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col4create">
+            <div class="col-lg-12 col12create text-center">
+            </div>
+        </div>
     </div>
-    <div class="col-lg-4 col4create">
-      <div class="col-lg-12 col12create text-center">
-      <h4>Create a New Chore List</h4>
-      <br><br>
-      <div class="dropdown">
-        <button class="btn btn-info dropdown-toggle" type="button" data-toggle="dropdown">Categories
-        <span class="caret"></span></button>
-        <ul class="dropdown-menu">
-          <li class="text-center"><a href="{{ url('userpages.chorelisthome') }}">Household</a></li>
-          <li class="text-center"><a href="{{ url('userpages.chorelistjob') }}">Job</a></li>
-          <li class="text-center"><a href="{{ url('userpages.chorelistentertainment') }}">Entertainment</a></li>
-        </ul>
-      </div>
-      </div>
-    </div>
-    <div class="col-lg-4 col4create">
-      <div class="col-lg-12 col12create text-center">
-      </div>
-    </div>
-  </div>
 </div>
 <h1>ARCHIVE</h1>
 <div class="container-fluid">
-  <div class="row">
+    <div class="row">
+        <div class="col-lg-4 col4created">
+            <div class="text-center">
+                <a href="#" class="btn btn-lg btn1 btn-block">
+                    shopping
+                </a>
+            </div>
+        </div>
     <div class="col-lg-4 col4created">
-      <div class="col-lg-6 col12created1 text-center">
-        shopping
-      </div>
+        <div class="text-center">
+            <a href="#" class="btn btn-lg btn2 btn-block">
+                chore
+            </a>
+        </div>
     </div>
     <div class="col-lg-4 col4created">
-      <div class="col-lg-6 col12created2 text-center">
-        chore
-      </div>
+        <div class="text-center">
+            <a href="#" class="btn btn-lg btn3 btn-block">
+                something else
+            </a>
+        </div>
     </div>
-    <div class="col-lg-4 col4created">
-      <div class="col-lg-6 col12created1 text-center">
-        something else
-      </div>
-    </div>
-  </div>
-</div><br>
+</div>
+<br>
 
 
 @endsection
