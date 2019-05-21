05d69ab17353825a71a2251184816c8e5761ff2a
zvvysotskaya <zhannavyvy@gmail.com>
2019-05-21 00:37:43 -0400

    transitions added to img

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,32 +1,39 @@
 @extends('layouts.app')
 @section('content')
 @section('title', 'home')
-<div id="whitebackground">
-  <hr id="hrGreen">
-    <div id="box1"><img src="images/welcomeFlower.jpg" width="74" alt="welcome flower"></div>
-      <div id="box2"><h1>Welcome to My Busy Life!</h1></div>
-    <div id="box3"><img src="images/welcomeFlower.jpg" width="74" alt="welcome flower"></div>
-  <hr id="hrGreen">
+
+<hr id="hrGreen">
+<div id="whitebackground" class="d-flex">
+    <div  class="col-2 offset-1 float-left imageHome">
+        <img src="images/welcomeFlower.jpg"  alt="welcome flower" class="img-fluid float-left d-block ">
+    </div>
+    <div class="col-6 text-center">
+        <h1>Welcome to My Busy Life!</h1>
+    </div>
+    <div class="col-2 offset-1 float-left imageHome">
+        <img src="images/welcomeFlower.jpg" alt="welcome flower" class="img-fluid d-block float-left">
+    </div>
 </div>
-<br>
-<br>
-<h2>Register to My Busy Life today, and you can build online your family budget,
-  create a shopping list, create a chore list, and more.</h2>
-  <div class="text-center">
+<hr id="hrGreen" class="mb-5">
+<div class="container">
+    <h2>Register to My Busy Life today, and you can build online your family budget,
+        create a shopping list, create a chore list, and more.
+    </h2>
+    <div class="text-center">
       @if (Route::has('login'))
           @auth
               <form action="{{'/home'}}">
                   <button type="submit" class="btn btn-info">Back To Your Account?</button>
               </form><br>
           @endauth
-       @endif
+          @endif
              @if(Route::has('register'))
-<form action="{{ route('register') }}">
-  <button type="submit" class="btn btn-danger">Register<span class="spinner-grow spinner-grow-sm text-warning"></span></button>
-</form>
+                <form action="{{ route('register') }}">
+                  <button type="submit" class="btn btn-danger">Register<span class="spinner-grow spinner-grow-sm text-warning"></span></button>
+                </form>
             @endif
-
 <br><br><br><br>
-
+    </div>
 </div>
+
 @endsection
