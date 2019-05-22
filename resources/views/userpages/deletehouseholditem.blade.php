7202838b261869f1116d43eca0321d15f8d496dd
zvvysotskaya <zhannavyvy@gmail.com>
2019-05-22 19:34:35 -0400

    col changed

--- resources/views/userpages/deletehouseholditem.blade.php
+++ resources/views/userpages/deletehouseholditem.blade.php
@@ -1,24 +1,16 @@
 @extends('layouts.itemheader')
 @section('content')
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-lg-3 col4create">
+<div class="bg-tb mt-5 py-4 mt-md-0 mt-sm-0">
+    <div class="container">
+        <div class="bg-box">
+            <h4>Item name: {{ $householditem->itemname }}</h4>
+            <h2>Delete?</h2>
+            <form method="POST" action="/userpages/{{ $householditem->id }}/deletehouseholditem" class="text-center">
+                @method('DELETE')
+                @csrf
+                <input type="submit" class="btn btn-danger" value="Delete">
+            </form>
         </div>
-        <div class="col-lg-6 col4create">
-            <div class="col-lg-12 col12create">
-                <div class="container">
-                    <h4>Item name: {{ $householditem->itemname }}</h4>
-                    <h2>Delete?</h2>
-                    <form method="POST" action="/userpages/{{ $householditem->id }}/deletehouseholditem" class="text-center">
-                        @method('DELETE')
-                        @csrf
-                        <input type="submit" class="btn btn-danger" value="Delete">
-                    </form>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col4create">
-        </div>
-    </div>
+    </div> 
 </div>
 @endsection
--- resources/views/userpages/shoppinglist.blade.php
+++ resources/views/userpages/shoppinglist.blade.php
@@ -20,113 +20,94 @@
 @endauth
 
 <!--display a groecery list-->
-
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-lg-3 col4create">
-        </div>
-        <div class="col-lg-6 col4create">
-            <div class="col-lg-12 col12create">
-                <h4>A Grocery List</h4>
-                    <div class="container">
-                        <table class="table table-striped">
-                              <thead>
-                                  <tr>
-                                      <th>Grocery Name</th>
-                                      <th>Quantity</th>
-                                      <th>Description</th>
-                                  </tr>
-                              </thead>
-                              @foreach($groceries as $grocery)
-                              <tbody>
-                                  <th>{{$grocery->groceryname}}</th>
-                                  <th>{{$grocery->quantity}}</th>
-                                  <th>{{$grocery->description}}</th>
-                              </tbody>
-                              @endforeach
-                        </table>
-                    </div>
-                    <form method="POST" action="{{ url('userpages/shoppinglist') }}">
-                        @method('DELETE')
-                        {{ csrf_field() }}
-                        <button type="submit" class="btn btn-warning">Delete All</button>
-                    </form>
-                </div>
-            </div>
-            <div class="col-lg-3 col4create">
-        </div>
-    </div>
+<div class="bg-tb mt-5 py-4">
+    <div class="container">
+        <div class="bg-box">
+            <h4>A Grocery List</h4>
+            <div class="container">
+                <table class="table table-hover ">
+                    <thead class="table-warning">
+                         <tr >
+                             <th class="text-center">Grocery Name</th>
+                             <th class="text-center">Quantity</th>
+                             <th class="text-center">Description</th>
+                          </tr>
+                      </thead>
+                      @foreach($groceries as $grocery)
+                      <tbody class="table_shoppinglist">
+                          <th class="text-center">{{$grocery->groceryname}}</th>
+                          <th class="text-center">{{$grocery->quantity}}</th>
+                          <th class="text-center" >{{$grocery->description}}</th>
+                      </tbody>
+                      @endforeach
+                  </table>
+              </div>
+              <div class="text-center">
+                  <form method="POST" action="{{ url('userpages/shoppinglist') }}">
+                      @method('DELETE')
+                      {{ csrf_field() }}
+                      <button type="submit" class="btn btn-warning">Delete All</button>
+                  </form>
+              </div>
+          </div>
+      </div>
 </div>
-
 <!--Display  a clothing list-->
-
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-lg-3 col4create">
-        </div>
-        <div class="col-lg-6 col4create">
-            <div class="col-lg-12 col12create">
-                <h4>A Clothing List</h4>
-                    <div class="container">
-                        <table class="table table-striped">
-                              <thead>
-                                  <tr>
-                                      <th>Clothing Items</th>
-                                      <th>Quantity</th>
-                                      <th>Description</th>
-                                  </tr>
-                              </thead>
-                              <!--
-                              @foreach($groceries as $grocery)
-                              <tbody>
-                                  <th>{{$grocery->groceryname}}</th>
-                                  <th>{{$grocery->quantity}}</th>
-                                  <th>{{$grocery->description}}</th>
-                              </tbody>
-                              @endforeach
-                          -->
-                        </table>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col4create">
-        </div>
-    </div>
-</div>
+<div class="bg-tb mt-5 py-4 mt-md-0 mt-sm-0">
+    <div class="container">
+        <div class="bg-box">
+            <h4>A Clothing List</h4>
+            <div class="container">
+                <table class="table table-hover">
+                    <thead class="table-warning">
+                        <tr>
+                             <th class="text-center">Clothing Items</th>
+                             <th class="text-center">Quantity</th>
+                             <th class="text-center">Description</th>
+                         </tr>
+                     </thead>
+                     <!--
+                     @foreach($groceries as $grocery)
+                     <tbody>
+                         <th>{{$grocery->groceryname}}</th>
+                         <th>{{$grocery->quantity}}</th>
+                         <th>{{$grocery->description}}</th>
+                     </tbody>
+                     @endforeach -->
+                 </table>
+             </div>
+         </div>
+     </div>
+ </div>
 
 <!--Display a shopping list for household items-->
 
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-lg-3 col4create">
-        </div>
-        <div class="col-lg-6 col4create">
-            <div class="col-lg-12 col12create">
-                <h4>Household Items</h4>
-                    <div class="container">
-                        <table class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th>Houshold Item Name</th>
-                                    <th>Quantity</th>
-                                    <th>Description</th>
-                                </tr>
-                            </thead>
-                            @foreach($householditems as $householditem)
-                            <tbody>
-                                <th>{{$householditem->itemname}}</th>
-                                <th>{{$householditem->quantity}}</th>
-                                <th>{{$householditem->description}}</th>
-                            </tbody>
-                            @endforeach
-                         </table>
-                    </div>
-                </div>
+<div class="bg-tb mt-5 py-4 mt-md-0 mt-sm-0">
+    <div class="container">
+        <div class="bg-box">
+            <h4>Household Items</h4>
+            <div class="container">
+                <table class="table table-hover">
+                    <thead class="table-warning">
+                        <tr>
+                            <th class="text-center">Houshold Item Name</th>
+                            <th class="text-center">Quantity</th>
+                            <th class="text-center">Description</th>
+                        </tr>
+                    </thead>
+                    @foreach($householditems as $householditem)
+                    <tbody class="table_shoppinglist">
+                        <th class="text-center">{{$householditem->itemname}}</th>
+                        <th class="text-center">{{$householditem->quantity}}</th>
+                        <th class="text-center">{{$householditem->description}}</th>
+                    </tbody>
+                    @endforeach
+                </table>
             </div>
-            <div class="col-lg-3 col4create">
+
         </div>
     </div>
 </div>
 
-{{date('m-d-y')}}
+
 @endsection
