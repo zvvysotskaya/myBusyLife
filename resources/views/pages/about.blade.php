0b617a34663af1e111408cfc8a1ab40f5da7f7c0
zvvysotskaya <zhannavyvy@gmail.com>
2019-06-06 00:41:03 -0400

    an about page changed

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,9 +2,11 @@
 @section('content')
 @section('title', 'about')
 <div class="container">
-    <div class="bg-box">
+    <div class="bg-box p-4">
         <h1>About My Busy Life App</h1>
-        <p>While registed you can create your own shopping list, a household chore list, and an intertainment list for you or your family to keep track of your or your family ideas. 
+        <p>While registed you can create your own shopping list, a household chore list, and an intertainment list for you or your family to keep
+            track of your or your family ideas.
+        </p>
     </div>
 
 </div>
