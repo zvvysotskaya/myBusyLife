1b4aee38a508c1e82927383aa7a3505d928bb59d
zvvysotskaya <zhannavyvy@gmail.com>
2019-06-08 15:08:07 -0400

    added backgraount to the about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 @section('title', 'about')
 <div class="container">
-    <div class="bg-box p-4">
+    <div class="bg_box p-4">
         <h1>About My Busy Life App</h1>
         <p>While registed you can create your own shopping list, a household chore list, and an intertainment list for you or your family to keep
             track of your or your family's ideas and needs.
